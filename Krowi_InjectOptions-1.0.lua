09b81830929808e405ebe9175c33fa346de7e38f
Krowi <Krowi@Home>
2023-07-23 23:07:37 +0200

    inject options

--- Krowi_InjectOptions-1.0.lua
+++ Krowi_InjectOptions-1.0.lua
@@ -23,8 +23,68 @@ lib.InjectOptions = {};
 local injectOptions = lib.InjectOptions;
 
 injectOptions.__index = injectOptions;
-function injectOptions:New(text)
+function injectOptions:New(optionsTable, defaultOptions, widthMultiplier)
     local instance = setmetatable({}, injectOptions);
-    instance.text = text;
+    instance.OptionsTable = optionsTable;
+    instance.DefaultOptions = defaultOptions;
+
+    instance.AdjustedWidth = function(number)
+        return (number or 1) * widthMultiplier or 170; -- Default Ace3
+    end
+
+    instance.autoOrder = 1;
+    instance.AutoOrderPlusPlus = function(amount)
+        local current = self.autoOrder;
+        self.autoOrder = self.autoOrder + (1 or amount);
+        return current;
+    end
+
+    instance.PlusPlusAutoOrder = function(amount)
+        self.autoOrder = self.autoOrder + (1 or amount);
+        return self.autoOrder;
+    end
+
     return instance;
+end
+
+function injectOptions:AddTable(destTablePath, key, table)
+    local destTable;
+    if type(destTablePath) == "table" then
+        destTable = destTablePath;
+    elseif type(destTablePath) == "string" then
+        destTable = self.OptionsTable.args;
+        local pathParts = strsplittable(".", destTablePath);
+        for _, part in next, pathParts do
+            destTable = destTable[part];
+        end
+    end
+    destTable[key] = table;
+    return destTable[key];
+end
+
+function injectOptions:TableExists(destTablePath)
+    local destTable = self.OptionsTable.args;
+    local pathParts = strsplittable(".", destTablePath);
+    for _, part in next, pathParts do
+        destTable = destTable[part];
+    end
+    return destTable and true or false;
+end
+
+function injectOptions:AddDefaults(destTablePath, key, table)
+    local destTable = self.DefaultOptions;
+    local pathParts = strsplittable(".", destTablePath);
+    for _, part in next, pathParts do
+        destTable = destTable[part];
+    end
+    destTable[key] = table;
+end
+
+function injectOptions:DefaultsExists(destTablePath)
+    local destTable = self.DefaultOptions;
+    local pathParts = strsplittable(".", destTablePath);
+    for _, part in next, pathParts do
+        destTable = destTable[part];
+    end
+    return destTable and true or false;
 end
